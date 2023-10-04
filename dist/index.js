9f372a606c27e7613e02de9c9ef8a15e7a677a9e
Joe Buza <joebm08@gmail.com>
2023-10-04 01:41:34 +0000

    Crud for categories

--- dist/index.js
+++ dist/index.js
@@ -1,80 +1,75 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import LRU from "lru-cache";
-var Category;
-(function (Category) {
-    Category["FICTION"] = "FICTION";
-    Category["NON_FICTION"] = "NON_FICTION";
-    Category["BIOGRAPHY"] = "BIOGRAPHY";
-    Category["SPORTS"] = "SPORTS";
-})(Category || (Category = {}));
+const BOOK_PREFIX = "book-";
+const CATEGORY_PREFIX = "cat-";
+const categories = [
+    { id: "cat-fiction", name: "Fiction" },
+    { id: "cat-non-fiction", name: "Non Fiction" },
+    { id: "cat-biography", name: "Biography" },
+    { id: "cat-sports", name: "Sports" },
+];
 const books = [
-    { title: "To Kill a Mockingbird", categories: [Category.FICTION] },
-    { title: "1984", categories: [Category.FICTION] },
-    { title: "Harry Potter and the Sorcerer's Stone", categories: [Category.FICTION] },
-    { title: "The Great Gatsby", categories: [Category.FICTION] },
-    { title: "The Diary of a Young Girl", categories: [Category.BIOGRAPHY] },
-    { title: "The Catcher in the Rye", categories: [Category.FICTION] },
-    { title: "Pride and Prejudice", categories: [Category.FICTION] },
-    { title: "The Hobbit", categories: [Category.FICTION] },
-    { title: "Moby Dick", categories: [Category.FICTION] },
-    { title: "War and Peace", categories: [Category.FICTION] },
-    { title: "The Odyssey", categories: [Category.FICTION] },
-    { title: "The Art of War", categories: [Category.NON_FICTION] },
-    { title: "A Brief History of Time", categories: [Category.NON_FICTION] },
-    { title: "The God Delusion", categories: [Category.NON_FICTION] },
     {
-        title: "Sapiens: A Brief History of Humankind",
-        categories: [Category.NON_FICTION],
-    },
-    { title: "Steve Jobs", categories: [Category.BIOGRAPHY] },
-    { title: "Long Walk to Freedom", categories: [Category.BIOGRAPHY] },
-    {
-        title: "The Autobiography of Martin Luther King, Jr.",
-        categories: [Category.BIOGRAPHY],
+        id: "book-tkmb",
+        title: "To Kill a Mockingbird",
+        categories: [categories[0]],
     },
     {
-        title: "Moneyball: The Art of Winning an Unfair Game",
-        categories: [Category.SPORTS],
+        id: "book-hpss",
+        title: "Harry Potter and the Sorcerer's Stone",
+        categories: [categories[0]],
     },
-    { title: "Open: An Autobiography", categories: [Category.BIOGRAPHY, Category.SPORTS] },
 ];
 const cache = LRU({ max: 25, maxAge: 1000 * 60 * 5 });
-export const generateIdFromTitle = (title) => title.split(" ").join("-").toLocaleLowerCase();
+export const genBookId = (title) => BOOK_PREFIX +
+    title
+        .split(" ")
+        .map((t) => t[0])
+        .join("")
+        .toLocaleLowerCase();
+export const genCategoryId = (name) => CATEGORY_PREFIX +
+    name.split(" ").join("-").toLocaleLowerCase().startsWith("cat-");
 // Preload the cache with the books
-books.forEach(({ title, categories }) => {
-    const id = generateIdFromTitle(title);
+books.forEach(({ id, title, categories }) => {
     cache.set(id, { title, categories, id });
+});
+categories.forEach(({ id, name }) => {
+    cache.set(id, { id, name });
 });
 // Schema definition
 const typeDefs = `
-  enum Category {
-    FICTION
-    NON_FICTION
-    BIOGRAPHY
-    SPORTS
-  }
-  
-  type Query {
-    books(filter: BookFilter): [Book]
-    book(id: String!): Book
-  }
+type Category {
+  id: String!
+  name: String!
+}
 
-  type Book {
-    id: String!
-    title: String!
-    categories: [Category]
-  }
+type Book {
+  id: String!
+  title: String!
+  categories: [Category]
+}
 
-  type Mutation {
-    addBook(title: String!, categories: [Category]): Book
-    updateBook(id: String!, title: String!, categories: [Category]): Book
-    deleteBook(id: String!): Boolean
-  }
+input BookFilter {
+  categoryId: String
+} 
 
-  input BookFilter {
-    category: Category
-  }
+type Query {
+  books(filter: BookFilter): [Book]
+  book(id: String!): Book
+  categories: [Category]
+  category(id: String!): Category
+}
+
+type Mutation {
+  addBook(title: String!, categoryIds: [String!]): Book
+  updateBook(id: String!, title: String!, categoryIds: [String!]): Book
+  deleteBook(id: String!): Boolean
+
+  addCategory(name: String!): Category
+  updateCategory(id: String!, name: String!): Category
+  deleteCategory(id: String!): Boolean
+}
 `;
 // Resolver definitions
 const resolvers = {
@@ -82,23 +77,35 @@ const resolvers = {
         books: (_, args) => {
             const books = [];
             cache.forEach((book, id) => {
-                if (!book)
+                if (!book || !id.startsWith(BOOK_PREFIX))
                     return;
                 books.push({ ...book, id });
             });
             if (args.filter && args.filter.category) {
-                const filterCategory = args.filter.category.toUpperCase();
-                return books.filter((book) => book.categories.map((c) => c.toUpperCase()).includes(filterCategory));
+                const categoryId = genCategoryId(args.filter.category);
+                return books.filter((book) => book.categories.map((c) => c.id === categoryId));
             }
             return books;
         },
         book: (_, { id }) => {
             return cache.get(id);
         },
+        categories: (_, args) => {
+            const categories = [];
+            cache.forEach((category, id) => {
+                if (!category || !id.startsWith(CATEGORY_PREFIX))
+                    return;
+                categories.push({ ...category, id });
+            });
+            return categories;
+        },
+        category: (_, { id }) => {
+            return cache.get(id);
+        },
     },
     Mutation: {
         addBook: (_, { title, categories }) => {
-            const id = generateIdFromTitle(title);
+            const id = genBookId(title);
             const book = { title, categories, id };
             cache.set(id, book);
             return book;
@@ -111,6 +118,22 @@ const resolvers = {
             return book;
         },
         deleteBook(_, { id }) {
+            return cache.set(id, undefined);
+        },
+        addCategory: (_, { title }) => {
+            const id = genCategoryId(title);
+            const category = { title, id };
+            cache.set(id, category);
+            return category;
+        },
+        updateCategory: (_, { title, id }) => {
+            if (!title)
+                return;
+            const category = { title, id };
+            cache.set(id, category);
+            return category;
+        },
+        deleteCategory(_, { id }) {
             return cache.set(id, undefined);
         },
     },
