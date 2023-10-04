763d6da112a7fe0703d820e7595fd5fc21e35425
Joe Buza <joebm08@gmail.com>
2023-10-04 01:43:47 +0000

    Update index.js and index.ts

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,8 @@ const categories = [
     { id: "cat-non-fiction", name: "Non Fiction" },
     { id: "cat-biography", name: "Biography" },
     { id: "cat-sports", name: "Sports" },
+    { id: "cat-science", name: "Science" },
+    { id: "cat-history", name: "History" },
 ];
 const books = [
     {
@@ -19,6 +21,26 @@ const books = [
         id: "book-hpss",
         title: "Harry Potter and the Sorcerer's Stone",
         categories: [categories[0]],
+    },
+    {
+        id: "book-1984",
+        title: "1984",
+        categories: [categories[0]],
+    },
+    {
+        id: "book-sapiens",
+        title: "Sapiens: A Brief History of Humankind",
+        categories: [categories[1]],
+    },
+    {
+        id: "book-mlk",
+        title: "The Autobiography of Martin Luther King, Jr.",
+        categories: [categories[2]],
+    },
+    {
+        id: "book-mjsm",
+        title: "Michael Jordan: The Life",
+        categories: [categories[3]],
     },
 ];
 const cache = LRU({ max: 25, maxAge: 1000 * 60 * 5 });
